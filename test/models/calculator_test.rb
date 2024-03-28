81e948d0e405b3111020e087a7361b51f04ab9b7
Ayushi Agrawal <ayushiagrawal0501@gmail.com>
2024-03-28 11:06:49 +0530

    Added test case for sum of given string

--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -3,5 +3,6 @@ require 'test_helper'
 class CalcString < ActiveSupport::TestCase
   test "it should return 0 for empty string" do
     assert_equal 0, Calculator.add("")
+    assert_equal 9, Calculator.add("2,3,4")
   end
 end
