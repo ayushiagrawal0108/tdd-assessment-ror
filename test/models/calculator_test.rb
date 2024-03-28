c59383cfe6cc915709133819e733e1eb6a2cb15c
Ayushi Agrawal <ayushiagrawal0501@gmail.com>
2024-03-28 10:40:20 +0530

    Created calculator file in models folder and added add method now test case is in green phase

--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class CalcString < ActiveSupport::TestCase
   test "it should return 0 for empty string" do
-    assert_equal 0, StringCalculator.add("")
+    assert_equal 0, Calculator.add("")
   end
 end
